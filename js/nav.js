9e51e51f37a64d5dc464a27b7403934ee269e563
Harris Foskolos <harrisfs.web@gmail.com>
2025-07-05 23:08:52 +0300

    added styles and menu

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+// js/nav.js
+// Injects a modern navigation bar into #main-nav
+
+const nav = document.getElementById('main-nav');
+if (nav) {
+    nav.innerHTML = `
+        <ul class="navbar">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="articles.html">Articles</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    `;
+}
